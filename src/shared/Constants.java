38293b08be640605368f0d14b63178ebc10c65d9
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-11-24 19:32:11 +0700

    Created Constants class for static variables

---
+++ src/shared/Constants.java
@@ -0,0 +1,13 @@
+package shared;
+
+import java.awt.Color;
+import java.awt.Insets;
+
+public class Constants
+{
+	//Colors
+	public static Color COLOR_OUT_OF_FOCUS = new Color(229, 233, 242);
+	
+	//Insets
+	public static Insets INSETS_GENERAL = new Insets(5, 5, 5, 5);
+}
