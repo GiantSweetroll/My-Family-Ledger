e13cee5d5fb6a341eb74e56ad73524dd6a813b32
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-11-27 12:18:57 +0700

    Added font in general size but bold

--- src/shared/Constants.java
+++ src/shared/Constants.java
@@ -21,6 +21,7 @@ public class Constants
 	public static final String FONT_TYPE_GENERAL = "lato";
 	public static final int FONT_GENERAL_SIZE = 20;
 	public static final Font FONT_GENERAL = new Font(FONT_TYPE_GENERAL, Font.PLAIN, FONT_GENERAL_SIZE);
+	public static final Font FONT_GENERAL_BOLD = new Font(FONT_TYPE_GENERAL, Font.BOLD, FONT_GENERAL_SIZE);
 	public static final Font FONT_SUB_TITLE = new Font(FONT_TYPE_GENERAL, Font.BOLD, FONT_GENERAL_SIZE*2);
 	public static final Font FONT_TITLE = new Font(FONT_TYPE_GENERAL, Font.BOLD, (int)GNumbers.round((float)FONT_GENERAL_SIZE*2.5f, 0));
 	public static final Font FONT_SMALLER = new Font(FONT_TYPE_GENERAL, Font.PLAIN, (int)GNumbers.round((float)FONT_GENERAL_SIZE*0.60f, 0));
