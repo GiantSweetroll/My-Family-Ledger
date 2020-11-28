1ff74c07116bcce8f92312da0ceb6b6c85818cff
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-11-28 08:56:50 +0700

    Added setter and getters

--- src/shared/screens/NameEmailPanel.java
+++ src/shared/screens/NameEmailPanel.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import models.Person;
 import shared.Constants;
 
 public class NameEmailPanel extends JPanel
@@ -34,6 +35,60 @@ public class NameEmailPanel extends JPanel
 		this.add(this.labEmail);
 	}
 	
+	//Public Methods
+	/**
+	 * Get the full name.
+	 * @return
+	 */
+	public String getFullName()
+	{
+		return this.labName.getText();
+	}
+	/**
+	 * Set the full name to be displayed.
+	 * @param name
+	 */
+	public void setFullName(String name)
+	{
+		this.labName.setText(name);
+	}
+	/**
+	 * Get the email text.
+	 * @return
+	 */
+	public String getEmail()
+	{
+		return this.labEmail.getText();
+	}
+	/**
+	 * Set the email text.
+	 * @param email
+	 */
+	public void setEmail(String email)
+	{
+		this.labEmail.setText(email);
+	}
+	/**
+	 * Set the name and email to be displayed. 
+	 * This method is equivalent to calling both the setFullName() and setEmail() methods.
+	 * @param name
+	 * @param email
+	 */
+	public void setNameEmail(String name, String email)
+	{
+		this.setFullName(name);
+		this.setEmail(email);
+	}
+	/**
+	 * Set the name and email to be displayed. The name and email will be taken from the Person object.
+	 * This method is equivalent to calling both the setFullName() and setEmail() methods.
+	 * @param person a Person class object.
+	 */
+	public void setNameEmail(Person person)
+	{
+		this.setFullName(person.getFullName());
+		this.setEmail(person.getEmail());
+	}
 	
 	//Testing
 	public static void main(String args[])
