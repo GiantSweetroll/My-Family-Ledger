fb621c83983f188b6325354bb8e474a46bfce2b6
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-12-09 11:11:07 +0700

    Added testing, font sizing, and better positioning

--- src/shared/screens/ListTile.java
+++ src/shared/screens/ListTile.java
@@ -3,12 +3,21 @@ package shared.screens;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.plaf.FontUIResource;
 
-public class ListTile extends RoundedPanel
+import shared.Constants;
+import shared.Methods;
+
+public class ListTile extends RoundedPanel implements MouseListener
 {
 
 	/**
@@ -22,18 +31,21 @@ public class ListTile extends RoundedPanel
 					labTopRight,
 					labTopLeft;
 	private JPanel panelTop, panelCenter;
+	private boolean isSelected;
 	
 	//Constructor
 	public ListTile()
 	{
 		//Initialization
 		super(false);
+		this.isSelected = false;
 		this.initPanelTop();
 		this.initPanelCenter();
 		
 		//Properties
 		this.setLayout(new BorderLayout());
 		this.setBackground(Color.WHITE);
+		this.addMouseListener(this);
 		
 		//Add to panel
 		this.add(this.panelTop, BorderLayout.NORTH);
@@ -81,11 +93,13 @@ public class ListTile extends RoundedPanel
 		this.panelTop = new JPanel(new BorderLayout());
 		this.labTopLeft = new JLabel();
 		this.labTopRight = new JLabel();
-		JPanel panelLeft = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		JPanel panelRight = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		JPanel panelLeft = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+		JPanel panelRight = new JPanel(new FlowLayout(FlowLayout.RIGHT, 10, 10));
 		
 		//Properties
 		this.panelTop.setOpaque(false);
+		this.labTopRight.setFont(Constants.FONT_SMALLER);
+		this.labTopLeft.setFont(Constants.FONT_SMALLER);
 		panelLeft.setOpaque(false);
 		panelRight.setOpaque(false);
 		
@@ -95,8 +109,8 @@ public class ListTile extends RoundedPanel
 		//Add to panelRight
 		panelRight.add(this.labTopRight);
 		//Add to panelTop
-		this.panelTop.add(this.labTopLeft, BorderLayout.WEST);
-		this.panelTop.add(this.labTopRight, BorderLayout.EAST);
+		this.panelTop.add(panelLeft, BorderLayout.WEST);
+		this.panelTop.add(panelRight, BorderLayout.EAST);
 	}
 	private void initPanelCenter()
 	{
@@ -108,9 +122,60 @@ public class ListTile extends RoundedPanel
 		//Properties
 		this.panelCenter.setLayout(new BoxLayout(this.panelCenter, BoxLayout.Y_AXIS));
 		this.panelCenter.setOpaque(false);
+		this.labTitle.setFont(Constants.FONT_GENERAL_BOLD);
+		this.labTitle.setBorder(BorderFactory.createEmptyBorder(1, 10, 1, 10));
+		this.labSubtitle.setFont(Constants.FONT_SMALLER);
+		this.labSubtitle.setBorder(BorderFactory.createEmptyBorder(1, 10, 1, 10));
 		
 		//Add to panel
 		this.panelCenter.add(this.labTitle);
 		this.panelCenter.add(this.labSubtitle);
+	}
+	
+	//Overridden Methods
+	@Override
+	public void mouseClicked(MouseEvent arg0) 
+	{
+		this.isSelected = !this.isSelected;
+		this.setBackground(this.isSelected? Constants.COLOR_SELECTED : Color.WHITE);
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {}
+
+	//Testing
+	public static void main(String arggs[])
+	{
+		Methods.setUIFont(new FontUIResource(Constants.FONT_TYPE_GENERAL, Font.PLAIN, Constants.FONT_GENERAL_SIZE));
+		
+		//Initialization
+		JFrame frame = new JFrame();
+		ListTile t1 = new ListTile();
+		ListTile t2 = new ListTile();
+		
+		//Properties
+		t1.setTitle("Venti");
+		t1.setSubtitle("Heppy birthday");
+		t1.setTopRightText("Rp. 250000");
+		t1.setTopLeftText("23/11/2000");
+		frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+		frame.setSize(700, 700);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Add to frame
+		frame.add(t1);
+		frame.add(t2);
+		
+		frame.setVisible(true);
 	}
 }
