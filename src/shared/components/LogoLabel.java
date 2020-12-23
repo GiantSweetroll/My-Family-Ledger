a4d02b46fa3fa5563a2243892c4d5228bcb48a16
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-12-23 12:01:19 +0700

    Made use of logo icon global constant, also added testing

--- src/shared/components/LogoLabel.java
+++ src/shared/components/LogoLabel.java
@@ -1,9 +1,13 @@
 package shared.components;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import java.awt.Font;
 
-import giantsweetroll.ImageManager;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.plaf.FontUIResource;
+
+import shared.Constants;
+import shared.Methods;
 
 public class LogoLabel extends JLabel
 {
@@ -15,8 +19,25 @@ public class LogoLabel extends JLabel
 	
 	public LogoLabel()
 	{
-		super();		//TODO: Replace with our actual logo
+		super(Constants.ICON_LOGO);
+	}
+	
+	//Testing
+	public static void main(String args[])
+	{
+		Methods.setUIFont(new FontUIResource(Constants.FONT_TYPE_GENERAL, Font.PLAIN, Constants.FONT_GENERAL_SIZE));
+		//Initialization
+		JFrame frame = new JFrame();
+		LogoLabel logo = new LogoLabel();
 		
-		this.setIcon(new ImageIcon(ImageManager.scaleImage(ImageManager.getImage("/resources/logo.png"), 300, 300)));
+		//Properties
+		frame.setSize(500, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Add to frame
+		frame.add(logo);
+		
+		frame.setVisible(true);
 	}
 }
