9840e5582cec717f3a99e25a42ab80f46bc42a73
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-12-23 17:01:11 +0700

    App can now start from here

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,15 +2,51 @@ package main;
 
 import java.awt.Font;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.plaf.FontUIResource;
 
+import screens.auth.SignIn;
 import shared.Constants;
 import shared.Methods;
 
 public class Main 
 {
+	//Fields
+	private static JFrame frame;
+	private static JPanel panel;
+	
+	//Constructor
+	public Main()
+	{
+		//Initialization
+		Main.frame = new JFrame();
+		Main.panel = new SignIn();
+		
+		//Properties
+		Main.frame.setSize(800, 600);
+		Main.frame.setLocationRelativeTo(null);
+		Main.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Add to frame
+		Main.frame.add(Main.panel);
+		
+		Main.frame.setVisible(true);
+	}
+	
+	//Public methods
+	public static void changeScreen(JPanel panel)
+	{
+		Main.frame.remove(Main.panel);
+		Main.panel = panel;
+		Main.frame.add(Main.panel);
+		Main.frame.revalidate();
+		Main.frame.repaint();
+	}
+	
 	public static void main(String args[])
 	{
 		Methods.setUIFont(new FontUIResource(Constants.FONT_TYPE_GENERAL, Font.PLAIN, Constants.FONT_GENERAL_SIZE));
+		new Main();
 	}
 }
