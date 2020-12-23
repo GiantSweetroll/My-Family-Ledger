ef498f0b338c0e99dc4cbfc287ebb206b37fd024
christynatalia <57931555+christynatalia@users.noreply.github.com>
2020-12-23 22:20:34 +0700

    change the size of logo

--- src/shared/Constants.java
+++ src/shared/Constants.java
@@ -47,7 +47,7 @@ public class Constants
 	//Icons
 	public static final ImageIcon ICON_DELETE = new ImageIcon(ImageManager.scaleImage(ImageManager.getImageIcon(Constants.class.getResource("/resources/trash.png")).getImage(), 15, 15));
 	public static final ImageIcon ICON_EDIT = new ImageIcon(ImageManager.scaleImage(ImageManager.getImageIcon(Constants.class.getResource("/resources/editicon.svg.png")).getImage(), 15, 15));
-	public static final ImageIcon ICON_LOGO = new ImageIcon(ImageManager.scaleImage(ImageManager.getImage(Constants.class.getResource("/resources/logo_trans.png")), 300, 300));
+	public static final ImageIcon ICON_LOGO = new ImageIcon(ImageManager.scaleImage(ImageManager.getImage(Constants.class.getResource("/resources/logo_trans.png")), 250, 250));
 	public static final ImageIcon ICON_LOGO_SMALL = new ImageIcon(ImageManager.scaleImage(ImageManager.getImage(Constants.class.getResource("/resources/logo_trans.png")), 100, 100));
 	
 	//Renderers
