2cfbb88d911bdc43b2da0cbf48cba800745a222d
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-12-24 20:04:25 +0700

    Added cross button icon constant

--- src/shared/Constants.java
+++ src/shared/Constants.java
@@ -53,6 +53,7 @@ public class Constants
 	public static final ImageIcon ICON_EDIT = new ImageIcon(ImageManager.scaleImage(ImageManager.getImageIcon(Constants.class.getResource("/resources/editicon.svg.png")).getImage(), 15, 15));
 	public static final ImageIcon ICON_LOGO = new ImageIcon(ImageManager.scaleImage(ImageManager.getImage(Constants.class.getResource("/resources/logo_trans.png")), 300, 300));
 	public static final ImageIcon ICON_LOGO_SMALL = new ImageIcon(ImageManager.scaleImage(ImageManager.getImage(Constants.class.getResource("/resources/logo_trans.png")), 100, 100));
+	public static final ImageIcon ICON_CLOSE = new ImageIcon(ImageManager.scaleImage(ImageManager.getImage("/resources/closeicon.png"), 16, 16));
 	
 	//Renderers
 	public static final IconCellRenderer RENDERER_DELETE = new IconCellRenderer(Constants.ICON_DELETE);
