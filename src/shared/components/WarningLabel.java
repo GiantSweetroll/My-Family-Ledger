712651a56c78cb869269a43f498b3a0f3d7ee0d3
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-12-24 12:02:38 +0700

    Create WarningLabel.java

---
+++ src/shared/components/WarningLabel.java
@@ -0,0 +1,22 @@
+package shared.components;
+
+import java.awt.Color;
+
+import javax.swing.JLabel;
+
+public class WarningLabel extends JLabel
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6560841143672525036L;
+	
+	//Constructor
+	public WarningLabel()
+	{
+		super();
+		
+		this.setForeground(Color.RED);
+	}
+}
