a978a4b2a58bae969ab02289489710fa41b8d3bc
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-12-30 16:03:49 +0700

    Create AmountCellRenderer.java

---
+++ src/shared/components/AmountCellRenderer.java
@@ -0,0 +1,62 @@
+package shared.components;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+import shared.Constants;
+
+public class AmountCellRenderer extends JLabel implements TableCellRenderer
+{
+	//Constructor
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 203724898768156128L;
+	
+	public AmountCellRenderer()
+	{
+		super();
+		this.setOpaque(true);
+	}
+	
+	//Overridden Methods
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object obj, boolean selected, boolean focused, int row,
+			int col)
+	{	
+		//Table row color pattern
+		if (row%2==1)
+		{
+			this.setBackground(Color.WHITE);
+		}
+		else
+		{
+			this.setBackground(Constants.COLOR_TABLE_EVEN_ROW);
+		}
+		
+		//Decide foreground color
+		boolean isNegative = false;
+		String amount = (String)obj;
+		this.setText(amount);
+		
+		if (amount.length() > 0)
+		{
+			isNegative = amount.substring(0, 1).equals("-");
+		}
+		
+		if(isNegative)
+		{
+			this.setForeground(Color.RED);
+		}
+		else
+		{
+			this.setForeground(Color.BLUE);
+		}
+		
+		return this;
+	}
+}
