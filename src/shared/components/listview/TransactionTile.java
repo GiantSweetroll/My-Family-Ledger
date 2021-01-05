937edb5ab532e282b1eab1c7fd0dc39a434a16a9
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-01-05 11:33:19 +0700

    Made some private field and method protected

--- src/shared/components/listview/TransactionTile.java
+++ src/shared/components/listview/TransactionTile.java
@@ -1,5 +1,7 @@
 package shared.components.listview;
 
+import java.awt.Color;
+
 import models.Transaction;
 import shared.Constants;
 
@@ -11,7 +13,7 @@ public class TransactionTile extends ListTile
 	 */
 	private static final long serialVersionUID = 3733432221115823308L;
 	//Fields
-	private Transaction transaction;
+	protected Transaction transaction;
 	
 	//Constructor
 	public TransactionTile(Transaction tr)
@@ -43,12 +45,14 @@ public class TransactionTile extends ListTile
 		return this.transaction;
 	}
 	
-	//Private methods
-	private void updateData()
+	//Protected methods
+	protected void updateData()
 	{
 		this.setTitle(Constants.DATABASE_SERVICE.getCategoryName(this.transaction.getCategoryID()));		//TODO: use DatabaseService to get Category name
 		this.setSubtitle(this.transaction.getDesc());
 		this.setTopLeftText(Constants.DATE_FORMAT.format(this.transaction.getDateInput()));
 		this.setTopRightText("Rp. " + this.transaction.getAmount());
+		
+		this.labTopRight.setForeground(this.transaction.getAmount() < 0 ? Color.RED : Color.BLUE);
 	}
 }
